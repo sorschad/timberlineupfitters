6336beec7115d0dede6ea14909e27c1681d33a31
SORS Chad <chad@southernoffroad.net>
2025-10-14 16:49:18 -0400

    Revise brands page to simplify

--- frontend/app/brands/page.tsx
+++ frontend/app/brands/page.tsx
@@ -286,9 +286,7 @@ export default async function BrandsPage() {
             {brand.vehicles && brand.vehicles.length > 0 && (
               <div className="mt-12">
                 <div className="text-left mb-3">
-                  <h3 className={`text-2xl font-semibold font-orbitron ${brandColors.text} mb-4`}>
-                    {brand.name} Vehicles
-                  </h3>
+                  
                 </div>
                 
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
