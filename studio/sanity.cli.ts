f59c64a2e35f7c61811741f8d7ae23064216a892
SORS Chad <chad@southernoffroad.net>
2025-10-29 11:56:53 -0400

    Add app id to sanity studio cli config

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -17,6 +17,7 @@ export default defineCliConfig({
   },
   studioHost: process.env.SANITY_STUDIO_STUDIO_HOST || '', // Visit https://www.sanity.io/docs/environment-variables to learn more about using environment variables for local & production.
   deployment: {
+    appId: 'j6jdugoyc09jz7rjbdc0ss2c',
     autoUpdates: true
   }
 })
